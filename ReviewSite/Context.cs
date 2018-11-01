6afab9845cdb3ff7a6a8a3bbf270309f0be5c6a5
khammer999 <khammer999@gmail.com>
2018-11-01 01:36:56 -0400

    20181101 0136hrs

--- ReviewSite/Context.cs
+++ ReviewSite/Context.cs
@@ -16,14 +16,18 @@ namespace ReviewSite
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category() { Id = 1, Name = "Music" },
-                new Category() { Id = 2, Name = "Book" }
+                new Category() { Id = 1, Name = "Concert" },
+                new Category() { Id = 2, Name = "Book" },
+                new Category() { Id = 3, Name = "Food"},
+                new Category() { Id = 4, Name = "Album"}
                 );
            
             modelBuilder.Entity<Review>().HasData(
-                new Review() { Id = 1, CategoryId = 1, Title = "Hard Rain at Natalies Coal Fired Pizza" },
-                new Review() { Id = 2, CategoryId = 2, Title = "Hunt for Red Oktober" },
-                new Review() { Id = 3, CategoryId = 2, Title = "Of Mice and Men" }
+                new Review() { Id = 1, CategoryId = 1, Title = "Hard Rain at Natalies Coal Fired Pizza", ImageUrl = "./www.root/images/NemoMuseum.jpg", Content = "Hard Rain rocked the house to a sold out crowd Friday 10-19-18" },
+                new Review() { Id = 2, CategoryId = 2, Title = "Hunt for Red Oktober", ImageUrl = "./www.root/images/bookcover.jpg", Content = "The classic by Tom Clancy that inpired the movie of the same name!" },
+                new Review() { Id = 3, CategoryId = 2, Title = "Of Mice and Men", ImageUrl = "./www.root/images/bookcover.jpg", Content = "the timeless John Steinbeck classic" },
+                new Review() { Id = 4, CategoryId = 4, Title = "Deep In The Shadows", ImageUrl = "./www.root/images/album.jpg", Content = "Blue Eyed Soul, Rythem and Blues from \"The PGH Rockers\" " },
+                new Review() { Id = 5, CategoryId = 3, Title = " Double Philly Cheese Steak Deluxe from Mr. Hero", ImageUrl = "./www.root/images/piggyLunch.jpg", Content = "If grease brings you gastronomic peace, then this dish should be your wish!"}
                 );
             base.OnModelCreating(modelBuilder);
         }
--- ReviewSite/Controllers/CategoryController.cs
+++ ReviewSite/Controllers/CategoryController.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ReviewSite.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: /<controller>/
+        private ICategoryRepository categoryRepo;
+
+        public CategoryController(ICategoryRepository categoryRepo)
+        {
+            this.categoryRepo = categoryRepo;
+        }
+
         public ViewResult Index()
         {
-
             var model = categoryRepo.GetAll();
             return View(model);
 
         }
+
+        public ViewResult Details(int id)
+        {
+            var model = categoryRepo.FindOneCategoryById(id);
+            return View(model);
+
+        }
+
+     
     }
 }
